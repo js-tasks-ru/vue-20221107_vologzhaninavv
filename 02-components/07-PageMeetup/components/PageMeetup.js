d2691165ac551fe031e4f80ce9baa3ebaea4e77a
Vologzhanina Viktoria <vologzhaninavv@gmail.com>
2022-11-16 23:17:11 +0300

    02/07: Add solution PageMeetup

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from '../meetupService.js';
+import MeetupView from '../../06-MeetupView/components/MeetupView.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,53 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      meetup: {},
+      loading: false,
+      error: false,
+    };
+  },
+
+  watch: {
+    meetupId: {
+      immediate: true,
+      async handler(id) {
+        this.loading = true;
+
+        try {
+          this.error = '';
+          this.meetup = await fetchMeetupById(id);
+        } catch (e) {
+          this.error = e.message;
+          this.meetup = {};
+        }
+
+        this.loading = false;
+      },
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <MeetupView v-if="!loading && !error" :meetup="meetup" />
 
-      <UiContainer>
+      <UiContainer v-else-if="loading">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-if="error">
+        <UiAlert>{{ error }}</UiAlert>
       </UiContainer>
     </div>`,
 });
