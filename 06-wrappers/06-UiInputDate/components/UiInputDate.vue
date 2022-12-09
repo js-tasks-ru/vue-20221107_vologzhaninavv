40303899df4c114d8677d9861a885b1bf1fa2fb2
Vologzhanina Viktoria <vologzhaninavv@gmail.com>
2022-12-10 00:24:39 +0300

    06/06: Add solution UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,9 @@
 <template>
-  <ui-input />
+  <ui-input v-bind="$attrs" v-model="value" :type="type" :valueAsNumber="valueAsNumber" @input="inputDate">
+    <template v-for="slotName in Object.keys($slots)" #[slotName]>
+      <slot :name="slotName" />
+    </template>
+  </ui-input>
 </template>
 
 <script>
@@ -9,5 +13,55 @@ export default {
   name: 'UiInputDate',
 
   components: { UiInput },
+
+  inheritAttrs: false,
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator: (val) => ['date', 'time', 'datetime-local'].includes(val),
+    },
+    modelValue: {
+      type: Number,
+      default: null,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    value() {
+      if (!this.modelValue) {
+        return '';
+      }
+
+      let valByType = new Date(this.modelValue).toISOString();
+
+      if (this.type === 'date') {
+        valByType = valByType.substring(0, 10);
+      } else if (this.type === 'time') {
+        valByType = valByType.substring(11, 16);
+      } else if (this.type === 'datetime-local') {
+        valByType = valByType.substring(0, 16);
+      }
+
+      return valByType;
+    },
+    valueAsNumber: {
+      get() {
+        return new Date(this.modelValue).getTime();
+      },
+      set(val) {
+        this.$emit('update:modelValue', val);
+      },
+    },
+  },
+
+  methods: {
+    inputDate(e) {
+      this.valueAsNumber = e.target.valueAsNumber;
+    },
+  },
 };
 </script>
