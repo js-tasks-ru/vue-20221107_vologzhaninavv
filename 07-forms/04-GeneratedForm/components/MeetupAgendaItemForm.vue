bbe5ced6cafd5a7eb0eda75a7b6437fc96fb4050
Vologzhanina Viktoria <vologzhaninavv@gmail.com>
2022-12-12 22:25:32 +0300

    07/04: Add solution GeneratedForm

--- 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -1,31 +1,28 @@
 <template>
   <fieldset class="agenda-item-form">
-    <button type="button" class="agenda-item-form__remove-button">
+    <button type="button" class="agenda-item-form__remove-button" @click="removeAgendaItem">
       <ui-icon icon="trash" />
     </button>
 
     <ui-form-group>
-      <ui-dropdown title="Тип" :options="$options.agendaItemTypeOptions" name="type" />
+      <ui-dropdown v-model="localAgendaItem.type" title="Тип" :options="$options.agendaItemTypeOptions" name="type" />
     </ui-form-group>
 
     <div class="agenda-item-form__row">
       <div class="agenda-item-form__col">
         <ui-form-group label="Начало">
-          <ui-input type="time" placeholder="00:00" name="startsAt" />
+          <ui-input v-model="localAgendaItem.startsAt" type="time" placeholder="00:00" name="startsAt" />
         </ui-form-group>
       </div>
       <div class="agenda-item-form__col">
         <ui-form-group label="Окончание">
-          <ui-input type="time" placeholder="00:00" name="endsAt" />
+          <ui-input v-model="localAgendaItem.endsAt" type="time" placeholder="00:00" name="endsAt" />
         </ui-form-group>
       </div>
     </div>
 
-    <ui-form-group label="Заголовок">
-      <ui-input name="title" />
-    </ui-form-group>
-    <ui-form-group label="Описание">
-      <ui-input multiline name="description" />
+    <ui-form-group v-for="(field, fieldName) in fieldsAgendaItem" :key="fieldName" :label="field.label">
+      <component :is="field.component" v-model="localAgendaItem[fieldName]" v-bind="field.props" :name="fieldName" />
     </ui-form-group>
   </fieldset>
 </template>
@@ -163,6 +160,44 @@ export default {
     agendaItem: {
       type: Object,
       required: true,
+    },
+  },
+
+  emits: ['update:agendaItem', 'remove'],
+
+  data() {
+    return {
+      localAgendaItem: { ...this.agendaItem },
+    };
+  },
+
+  computed: {
+    fieldsAgendaItem() {
+      return agendaItemFormSchemas[this.localAgendaItem.type];
+    },
+  },
+
+  watch: {
+    localAgendaItem: {
+      deep: true,
+      handler() {
+        this.$emit('update:agendaItem', { ...this.localAgendaItem });
+      },
+    },
+    'localAgendaItem.startsAt'(newVal, oldVal) {
+      const diff = this.getTimeInMs(this.localAgendaItem.endsAt) - this.getTimeInMs(oldVal);
+      const newEndsAtTime = this.getTimeInMs(newVal) + diff;
+      this.localAgendaItem.endsAt = new Date(newEndsAtTime).toISOString().substring(11, 16);
+    },
+  },
+
+  methods: {
+    removeAgendaItem() {
+      this.$emit('remove');
+    },
+    getTimeInMs(time) {
+      const [hours, minutes] = time.split(':');
+      return (parseInt(hours, 10) * 60 + parseInt(minutes, 10)) * 60 * 1000;
     },
   },
 };
