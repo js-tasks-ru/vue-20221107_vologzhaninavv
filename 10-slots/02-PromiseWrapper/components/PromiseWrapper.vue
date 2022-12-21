008e6b4c4a59f0638049b4e2ba834e008a9d6faa
Vologzhanina Viktoria <vologzhaninavv@gmail.com>
2022-12-21 22:12:21 +0300

    10/02: Add solution PromiseWrapper

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,7 @@
 <template>
-  <!-- -->
+  <slot name="fulfilled" v-if="result" :result="result"></slot>
+  <slot name="rejected" v-else-if="error" :error="error"></slot>
+  <slot name="pending" v-else></slot>
 </template>
 
 <script>
@@ -10,6 +12,37 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      result: null,
+      error: null,
+    };
+  },
+
+  watch: {
+    promise: {
+      immediate: true,
+      async handler() {
+        this.resetResultPromise();
+        await this.getResultPromise();
+      },
+    },
+  },
+
+  methods: {
+    async getResultPromise() {
+      try {
+        this.result = await this.promise;
+      } catch(e) {
+        this.error = e;
+      }
+    },
+    resetResultPromise() {
+      this.result = null;
+      this.error = null;
     },
   },
 };
